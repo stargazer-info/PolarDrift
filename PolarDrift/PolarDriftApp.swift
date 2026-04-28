4bdbca48d1f196db7d84f4e23487be3288a082ea
Nobuyuki Yamaguchi <nyama195@gmail.com>
2026-04-28 13:27:50 +0900

    Initial Commit

---
+++ PolarDrift/PolarDriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarDriftApp.swift
+//  PolarDrift
+//
+//  Created by 山口 伸行 on 2026/04/28.
+//
+
+import SwiftUI
+
+@main
+struct PolarDriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
